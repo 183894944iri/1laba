4f03ccd4d2c797f5d508ccc9d390894d40e4f047
183894944iri <113174903+183894944iri@users.noreply.github.com>
2023-12-21 01:10:24 +0300

    1

---
+++ ConsoleApp1/Point.cs
@@ -0,0 +1,24 @@
+namespace ConsoleApp1
+{
+    public class Point
+    {
+        private int _x;
+        private int _y;
+
+        public Point(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public double X
+        {
+            get { return _x; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+        }
+    }
+}
